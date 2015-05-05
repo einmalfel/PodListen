529eca610fe2499e63282e3077fc717a4044064b
Vasily Makarov <einmalfel@gmail.com>
2015-05-05 19:51:42 +0300

    add stub authenticator facility

---
+++ app/src/main/java/com/einmalfel/podlisten/StubAuthenticatorService.java
@@ -0,0 +1,19 @@
+package com.einmalfel.podlisten;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class StubAuthenticatorService extends Service {
+  private StubAuthenticator mAuthenticator;
+
+  @Override
+  public void onCreate() {
+    mAuthenticator = new StubAuthenticator(this);
+  }
+
+  @Override
+  public IBinder onBind(Intent intent) {
+    return mAuthenticator.getIBinder();
+  }
+}
