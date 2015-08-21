e142d6df0ea3f15e9fb4ec2b86f0ae9074a18278
Vasily Makarov <einmalfel@gmail.com>
2015-08-21 21:54:54 +0300

    upgrade to latest SDK

--- app/src/main/java/com/einmalfel/podlisten/support/RecyclerItemClickListener.java
+++ app/src/main/java/com/einmalfel/podlisten/support/RecyclerItemClickListener.java
@@ -50,4 +50,9 @@ public class RecyclerItemClickListener implements RecyclerView.OnItemTouchListen
   @Override
   public void onTouchEvent(RecyclerView view, MotionEvent motionEvent) {
   }
+
+  @Override
+  public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+  }
 }
