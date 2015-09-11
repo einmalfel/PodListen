362121b8b89bd1ca013be1a6e2da31d230037526
Vasily Makarov <einmalfel@gmail.com>
2015-09-11 20:47:25 +0300

    add Unit converter utility class

---
+++ app/src/main/java/com/einmalfel/podlisten/support/UnitConverter.java
@@ -0,0 +1,39 @@
+package com.einmalfel.podlisten.support;
+
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+
+import com.einmalfel.podlisten.PodListenApp;
+
+public class UnitConverter {
+  private static UnitConverter instance;
+  private final DisplayMetrics displayMetrics;
+
+  public static UnitConverter getInstance() {
+    if (instance == null) {
+      synchronized (UnitConverter.class) {
+        if (instance == null) {
+          instance = new UnitConverter();
+        }
+      }
+    }
+    return instance;
+  }
+
+  private UnitConverter() {
+    displayMetrics = PodListenApp.getContext().getResources().getDisplayMetrics();
+  }
+
+  public int spToPx(int dp) {
+    return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, dp, displayMetrics));
+  }
+
+  public int dpToPx(int dp) {
+    return Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, displayMetrics));
+  }
+
+  public int pxToDp(int px) {
+    return Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));
+  }
+
+}
