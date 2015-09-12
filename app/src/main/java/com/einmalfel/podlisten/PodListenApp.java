408e1e4caa7b23c5f57e926eb7a7086ec5cc91e0
Vasily Makarov <einmalfel@gmail.com>
2015-09-12 21:57:16 +0300

    add crash reporting via ACRA

--- app/src/main/java/com/einmalfel/podlisten/PodListenApp.java
+++ app/src/main/java/com/einmalfel/podlisten/PodListenApp.java
@@ -2,6 +2,21 @@ package com.einmalfel.podlisten;
 
 import android.content.Context;
 
+import org.acra.ACRA;
+import org.acra.ReportField;
+import org.acra.annotation.ReportsCrashes;
+
+@ReportsCrashes(
+    mailTo = "einmalfel@gmail.com",
+    logcatArguments = {"-t", "1000", "-v", "time", "*:S",
+                       "PLA:V", "DSR:V", "ELA:V", "SSA:V", "EVH:V", "IMG:V", "MAC:V", "NEF:V",
+                       "PLC:V", "PPS:V", "PLF:V", "EPM:V", "PLP:V", "SSF:V", "SWK:V", "WGH:V"},
+    customReportContent = {
+        ReportField.USER_CRASH_DATE, ReportField.AVAILABLE_MEM_SIZE, ReportField.TOTAL_MEM_SIZE,
+        ReportField.BUILD, ReportField.BUILD_CONFIG, ReportField.DISPLAY,
+        ReportField.CRASH_CONFIGURATION, ReportField.SHARED_PREFERENCES, ReportField.LOGCAT,
+        ReportField.STACK_TRACE}
+)
 public class PodListenApp extends DebuggableApp {
   private static PodListenApp instance;
 
@@ -16,6 +31,11 @@ public class PodListenApp extends DebuggableApp {
   @Override
   public void onCreate() {
     instance = this;
+    ACRA.init(this);
     super.onCreate();
+  }
+
+  static void sendLogs() {
+    ACRA.getErrorReporter().handleException(null);
   }
 }
