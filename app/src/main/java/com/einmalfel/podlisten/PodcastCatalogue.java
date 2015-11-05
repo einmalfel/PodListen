f4404cbef982f446dbe995c3650f70f2a7a96e5e
Vasily Makarov <einmalfel@gmail.com>
2015-11-05 15:47:07 +0300

    PodcastCatalogue: update search database only once

--- app/src/main/java/com/einmalfel/podlisten/PodcastCatalogue.java
+++ app/src/main/java/com/einmalfel/podlisten/PodcastCatalogue.java
@@ -1,7 +1,9 @@
 package com.einmalfel.podlisten;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
 
@@ -12,17 +14,30 @@ public class PodcastCatalogue {
     }
   }
 
+  static final String FTS_NAME = "catalogue_fts";
+  private static final String CATALOGUE_NAME = "catalogue";
   private static final String TAG = "PCT";
 
   final SQLiteDatabase db;
 
   public PodcastCatalogue(Context context) {
     db = new CatalogueHelper(context).getWritableDatabase();
-    db.execSQL(
-        "CREATE VIRTUAL TABLE IF NOT EXISTS catalogue_fts USING fts4(content='catalogue', " +
-            "_ID, title, description, rss_url, period, web_url)");
-    db.execSQL(
-        "INSERT OR REPLACE INTO catalogue_fts (docid, _ID, title, description, rss_url, period, web_url) " +
-            "SELECT _ID, _ID, title, description, rss_url, period, web_url FROM catalogue");
+    Cursor cursor = db.rawQuery("SELECT tbl_name FROM sqlite_master WHERE tbl_name == ?",
+                                new String[]{FTS_NAME});
+    if (cursor.getCount() == 0) {
+      Log.i(TAG, "Generating FTS DB");
+      db.beginTransaction();
+      try {
+        db.execSQL("CREATE VIRTUAL TABLE IF NOT EXISTS " + FTS_NAME + " USING fts4(content='" +
+                       CATALOGUE_NAME + "', _ID, title, description, rss_url, period, web_url)");
+        db.execSQL("INSERT OR REPLACE INTO " + FTS_NAME + " (docid, _ID, title, description, " +
+                       "rss_url, period, web_url) SELECT _ID, _ID, title, description, rss_url, " +
+                       "period, web_url FROM " + CATALOGUE_NAME);
+        db.setTransactionSuccessful();
+      } finally {
+        db.endTransaction();
+      }
+    }
+    cursor.close();
   }
 }
