einmalfel/PodListen

commit 111caab722dcae8668506958034d1036a10e27a0

delete eps in background service & show snackbar with "undo" button

app/src/main/java/com/einmalfel/podlisten/SyncWorker.java
@@ -3,7 +3,6 @@ package com.einmalfel.podlisten;
 import android.content.ContentProviderClient;
 import android.content.ContentValues;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.RemoteException;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -62,7 +61,6 @@ class SyncWorker implements Runnable {
 
   @Override
   public void run() {
-    String title = null;
     try {
       InputStream inputStream = openConnectionWithTO(new URL(link)).getInputStream();
       Feed feed = EarlParser.parseOrThrow(inputStream, MAX_EPISODES_TO_PARSE);
@@ -83,46 +81,23 @@ class SyncWorker implements Runnable {
         }
       }
 
-      title = updateFeed(id, feed, timestamp);
+      updateFeed(id, feed, timestamp);
 
       // delete every gone episode whose timestamp is less then feeds timestamp
-      if (title != null) {
-        Cursor episodesToDelete = provider.query(
-            Provider.episodeUri,
-            new String[]{Provider.K_ID},
-            Provider.K_ETSTAMP + " < ? AND " + Provider.K_ESTATE + " == ? AND " + Provider.K_EPID + " == ?",
-            new String[]{Long.toString(timestamp.getTime()),
-                         Integer.toString(Provider.ESTATE_GONE),
-                         Long.toString(id)},
-            null);
-        if (episodesToDelete != null) {
-          while (episodesToDelete.moveToNext()) {
-            long id = episodesToDelete.getLong(episodesToDelete.getColumnIndex(Provider.K_ID));
-            boolean result = PodcastHelper.getInstance().deleteEpisode(id);
-            Log.d(TAG, "Episode " + id + " is absent in updated feed and gone. Deleted: " + result);
-          }
-          episodesToDelete.close();
-        } else {
-          Log.e(TAG, "Failed to obtain episodesToDelete cursor");
-        }
+      PodcastOperations.cleanupEpisodes(PodListenApp.getContext(), Provider.ESTATE_GONE);
 
-        syncState.signalFeedSuccess(title, newEpisodesInserted);
-      } else {
-        storeFeedError(new Exception("Failed to update podcast data in db"));
-        syncState.signalDBError(link);
-      }
     } catch (IOException exception) {
       storeFeedError(exception);
       syncState.signalIOError(link);
     } catch (RemoteException exception) {
       storeFeedError(exception);
-      syncState.signalDBError(title == null ? link : title);
+      syncState.signalDBError(link);
     } catch (DataFormatException | XmlPullParserException exception) {
       storeFeedError(exception);
       syncState.signalParseError(link);
     } catch (Exception exception) {
       storeFeedError(exception);
-      syncState.signalIOError(title == null ? link : title);
+      syncState.signalIOError(link);
     }
   }
 
@@ -244,12 +219,7 @@ class SyncWorker implements Runnable {
     return true;
   }
 
-  /**
-   * @return feed title if success, null otherwise
-   * @throws RemoteException
-   */
-  @Nullable
-  private String updateFeed(long id, @NonNull Feed feed, @NonNull Date timestamp)
+  private void updateFeed(long id, @NonNull Feed feed, @NonNull Date timestamp)
       throws RemoteException {
     ContentValues values = new ContentValues();
     String title = feed.getTitle();
@@ -274,8 +244,9 @@ class SyncWorker implements Runnable {
         Log.w(TAG, image + ": Feed image download failed: ", exception);
       }
     }
-    int updated = provider.update(Provider.getUri(Provider.T_PODCAST, id), values, null, null);
-    return updated == 1 ? title : null;
+    if (provider.update(Provider.getUri(Provider.T_PODCAST, id), values, null, null) != 1) {
+      throw new RemoteException("Failed to update database with new podcast data");
+    }
   }
 
   @NonNull

app/src/main/AndroidManifest.xml
@@ -181,6 +181,11 @@
                 android:name="android.support.PARENT_ACTIVITY"
                 android:value="com.einmalfel.podlisten.MainActivity"/>
         </activity>
+
+        <service
+            android:name=".PodcastOperations"
+            android:exported="false">
+        </service>
     </application>
 
 </manifest>

app/src/main/java/com/einmalfel/podlisten/MainActivity.java
@@ -1,5 +1,7 @@
 package com.einmalfel.podlisten;
 
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -19,6 +21,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -342,6 +345,57 @@ public class MainActivity extends FragmentActivity implements PlayerService.Play
     });
   }
 
+  void deleteEpisodeDialog(final long episodeId) {
+    Cursor c = getContentResolver().query(Provider.getUri(Provider.T_EPISODE, episodeId),
+                                          new String[]{Provider.K_ENAME, Provider.K_ESTATE},
+                                          null, null, null);
+    if (c == null) {
+      Log.wtf(TAG, "Failed to delete " + episodeId + ". Got null from query");
+      return;
+    }
+    if (c.moveToFirst()) {
+      final String episodeName = c.getString(c.getColumnIndexOrThrow(Provider.K_ENAME));
+      final int prevState = c.getInt(c.getColumnIndexOrThrow(Provider.K_ESTATE));
+      new AlertDialog.Builder(this)
+          .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+              ContentValues cv = new ContentValues(1);
+              cv.put(Provider.K_ESTATE, Provider.ESTATE_LEAVING);
+              getContentResolver().update(
+                  Provider.getUri(Provider.T_EPISODE, episodeId), cv, null, null);
+              deleteEpisodeSnackbar(getString(R.string.episode_deleted, episodeName), prevState);
+            }
+          })
+          .setNegativeButton(R.string.cancel, null)
+          .setTitle(getString(R.string.episode_delete_question))
+          .create()
+          .show();
+    } else {
+      Log.e(TAG, "Trying to delete absent episode " + episodeId);
+    }
+    c.close();
+  }
+
+  void deleteEpisodeSnackbar(String title, final int prevState) {
+    showSnackbar(
+        title,
+        Snackbar.LENGTH_LONG,
+        getString(R.string.episode_deleted_undo),
+        new Snackbar.Callback() {
+          @Override
+          public void onDismissed(Snackbar snackbar, int event) {
+            Log.d(TAG, "Snackbar dismissed, event: " + event);
+            if (event == DISMISS_EVENT_ACTION) {
+              PodcastOperations.setEpisodesState(
+                  MainActivity.this, prevState, Provider.ESTATE_LEAVING);
+            } else {
+              PodcastOperations.cleanupEpisodes(MainActivity.this, Provider.ESTATE_LEAVING);
+            }
+          }
+        }
+    );
+  }
+
   private void showSnackbar(@NonNull String text, int duration, @Nullable String action,
                             @Nullable final Snackbar.Callback callback) {
     if (snackbar == null || !snackbar.isShownOrQueued()) {
@@ -383,7 +437,8 @@ public class MainActivity extends FragmentActivity implements PlayerService.Play
     if (v == fab) {
       switch (currentFabAction) {
         case CLEAR:
-          PodcastHelper.getInstance().clearNewEpisodes();
+          PodcastOperations.setEpisodesState(this, Provider.ESTATE_LEAVING, Provider.ESTATE_NEW);
+          deleteEpisodeSnackbar("New episodes cleaned", Provider.ESTATE_NEW);
           break;
         case ADD:
           openSubscribeDialog(null);

app/src/main/java/com/einmalfel/podlisten/NewEpisodesFragment.java
@@ -45,7 +45,7 @@ public class NewEpisodesFragment extends DebuggableFragment implements LoaderMan
 
   @Override
   public boolean onLongTap(long id) {
-    PodcastHelper.deleteEpisodeDialog(id, activity);
+    activity.deleteEpisodeDialog(id);
     return true;
   }
 

app/src/main/java/com/einmalfel/podlisten/PlayerService.java
@@ -612,7 +612,10 @@ public class PlayerService extends DebuggableService implements MediaPlayer.OnSe
     if (completeAction == Preferences.CompleteAction.DELETE_PLAY_FIRST ||
         completeAction == Preferences.CompleteAction.DELETE_PLAY_NEXT ||
         completeAction == Preferences.CompleteAction.DELETE_DO_NOTHING) {
-      PodcastHelper.getInstance().markEpisodeGone(currentId);
+      ContentValues cv = new ContentValues(1);
+      cv.put(Provider.K_ESTATE, Provider.ESTATE_GONE);
+      getContentResolver().update(Provider.getUri(Provider.T_EPISODE, currentId), cv, null, null);
+      PodcastOperations.cleanupEpisodes(this, Provider.ESTATE_GONE);
     }
 
     if (nextId == 0 || completeAction == Preferences.CompleteAction.DELETE_DO_NOTHING) {

app/src/main/java/com/einmalfel/podlisten/PlaylistFragment.java
@@ -67,7 +67,7 @@ public class PlaylistFragment extends DebuggableFragment implements
 
   @Override
   public boolean onLongTap(long id) {
-    PodcastHelper.deleteEpisodeDialog(id, activity);
+    activity.deleteEpisodeDialog(id);
     return true;
   }
 

app/src/main/java/com/einmalfel/podlisten/PodListenApp.java
@@ -18,7 +18,7 @@ import org.acra.annotation.ReportsCrashes;
     logcatArguments = {"-t", "1000", "-v", "time", "*:S",
                        "PLA:V", "DLR:V", "ELA:V", "SSA:V", "EVH:V", "IMG:V", "MAC:V", "NEF:V",
                        "PLC:V", "PPS:V", "PLF:V", "EPM:V", "PLP:V", "SSF:V", "SWK:V", "WGH:V",
-                       "SDF:V", "STB:V", "MBR:V", "PAC:V", "STR:V", "PRF:V", "PCT:V",
+                       "SDF:V", "STB:V", "MBR:V", "PAC:V", "STR:V", "PRF:V", "PCT:V", "POS:V",
                        "AndroidRuntime:V", "ACRA:V"},
     customReportContent = {
         ReportField.USER_CRASH_DATE, ReportField.AVAILABLE_MEM_SIZE, ReportField.TOTAL_MEM_SIZE,

app/src/main/java/com/einmalfel/podlisten/PodcastHelper.java
@@ -1,32 +1,27 @@
 package com.einmalfel.podlisten;
 
 
-import android.app.DownloadManager;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AlertDialog;
 import android.text.format.DateUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 import com.einmalfel.podlisten.support.UnitConverter;
 
-import java.io.File;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 /**
- * Helper class intended to do podcast-related stuff, like properly deleting episodes, etc
+ * Helper class intended to do podcast-related stuff
  */
 public class PodcastHelper {
   static final int MIN_IMAGE_WIDTH_SP = 70;
@@ -62,87 +57,8 @@ public class PodcastHelper {
     return (long) url.hashCode() - Integer.MIN_VALUE;
   }
 
-  public static void deleteEpisodeDialog(final long episodeId, final Context context) {
-    AlertDialog.Builder builder = new AlertDialog.Builder(context);
-    builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-      public void onClick(DialogInterface dialog, int id) {
-        PodcastHelper.getInstance().markEpisodeGone(episodeId);
-      }
-    });
-    builder
-        .setNegativeButton(R.string.cancel, null)
-        .setTitle(context.getString(R.string.episode_delete_question))
-        .create()
-        .show();
-  }
 
-  private void tryDeleteFiles(long id) {
-    Storage storage = Preferences.getInstance().getStorage();
-    if (storage == null || !storage.isAvailableRW()) {
-      Log.w(TAG, "failed to delete episode media: no storage or it isn't writable");
-      return;
-    }
 
-    File f = new File(storage.getPodcastDir(), Long.toString(id));
-    if (f.exists() && !f.delete()) {
-      Log.w(TAG, "Failed to delete " + f.toURI());
-    }
-
-    ImageManager.getInstance().deleteImage(id);
-  }
-
-  public boolean markEpisodeGone(long id) {
-    return markEpisodeGone(id, false);
-  }
-
-  /**
-   * Marks episode as gone.
-   * If episode is absent in origin feed, deletes it from DB.
-   * Tries to delete downloaded media (if there is some on mounted external storage)
-   *
-   * @param id episode id to delete
-   * @return true if success, false if episode is already absent in db
-   */
-  public boolean markEpisodeGone(long id, boolean quiet) {
-    boolean result = false;
-    // TODO what if subscription is deleted?
-    Cursor c = resolver.query(
-        Provider.getUri(Provider.T_E_JOIN_P, id),
-        new String[]{Provider.K_ETSTAMP, Provider.K_PTSTAMP, Provider.K_ENAME, Provider.K_EDID},
-        Provider.K_ESTATE + " != ?",
-        new String[]{Integer.toString(Provider.ESTATE_GONE)},
-        null);
-    if (c.moveToFirst()) {
-      if (!quiet) {
-        Toast.makeText(
-            context,
-            context.getString(R.string.episode_deleted,
-                              c.getString(c.getColumnIndex(Provider.K_ENAME))),
-            Toast.LENGTH_SHORT
-        ).show();
-      }
-      long dId = c.getLong(c.getColumnIndexOrThrow(Provider.K_EDID));
-      if (dId != 0) {
-        DownloadManager dm = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
-        dm.remove(dId);
-      }
-      if (c.getLong(c.getColumnIndexOrThrow(Provider.K_ETSTAMP)) < c.getLong(c.getColumnIndexOrThrow(Provider.K_PTSTAMP))) {
-        Log.i(TAG, "Feed doesn't contain episode " + id + " anymore. Deleting..");
-        c.close();
-        return deleteEpisode(id);
-      } else {
-        ContentValues val = new ContentValues(3);
-        val.put(Provider.K_ESTATE, Provider.ESTATE_GONE);
-        val.put(Provider.K_EDFIN, 0);
-        val.put(Provider.K_EDID, 0);
-        result = resolver.update(Provider.getUri(Provider.T_EPISODE, id), val, null, null) == 1;
-      }
-    }
-    c.close();
-    tryDeleteFiles(id);
-    return result;
-
-  }
 
   private static final DateFormat formatYYYYMMDD = new SimpleDateFormat("yyyy-MM-dd", Locale.US);
 
@@ -173,18 +89,6 @@ public class PodcastHelper {
     int exp = (int) (Math.log(bytes) / Math.log(unit));
     String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (si ? "" : "i");
     return String.format("%d%sB", (int) (bytes / Math.pow(unit, exp)), pre);
-  }
-
-  /**
-   * Try remove episode from db.
-   * Remove media files if any.
-   *
-   * @param id of episode
-   * @return true if successfully deleted from db
-   */
-  public boolean deleteEpisode(long id) {
-    tryDeleteFiles(id);
-    return resolver.delete(Provider.getUri(Provider.T_EPISODE, id), null, null) == 1;
   }
 
   public class SubscriptionNotInsertedException extends Throwable {
@@ -251,19 +155,5 @@ public class PodcastHelper {
     } else {
       return width;
     }
-  }
-
-  void clearNewEpisodes() {
-    Cursor c = context.getContentResolver().query(
-        Provider.episodeUri,
-        new String[]{Provider.K_ID},
-        Provider.K_ESTATE + " = ?",
-        new String[]{Integer.toString(Provider.ESTATE_NEW)},
-        null);
-    while (c.moveToNext()) {
-      PodcastHelper.getInstance().markEpisodeGone(
-          c.getLong(c.getColumnIndex(Provider.K_ID)), true);
-    }
-    c.close();
   }
 }

commit 45228e46d9e288d6b915d276b20ea51e4a887bfa

SyncWorker: fix refresh progress indication broken in 111caab722dcae

app/src/main/java/com/einmalfel/podlisten/SyncWorker.java
@@ -64,7 +64,7 @@ class SyncWorker implements Runnable {
       InputStream inputStream = openConnectionWithTO(new URL(link)).getInputStream();
       Feed feed = EarlParser.parseOrThrow(inputStream, MAX_EPISODES_TO_PARSE);
 
-      updateFeed(id, feed);
+      String title = updateFeed(id, feed);
 
       // Episodes need to be timestamped before subscriptions, otherwise cleanup algorithm may
       // delete fresh episodes in case of an exception between feed and episodes update
@@ -85,6 +85,7 @@ class SyncWorker implements Runnable {
       ContentValues values = new ContentValues(1);
       values.put(Provider.K_PTSTAMP, timestamp.getTime());
       if (provider.update(Provider.getUri(Provider.T_PODCAST, id), values, null, null) == 1) {
+        syncState.signalFeedSuccess(title, newEpisodesInserted);
         // delete every gone episode whose timestamp is less then feeds timestamp
         BackgroundOperations.cleanupEpisodes(PodListenApp.getContext(), Provider.ESTATE_GONE);
       } else {
@@ -219,7 +220,8 @@ class SyncWorker implements Runnable {
     return true;
   }
 
-  private void updateFeed(long id, @NonNull Feed feed)
+  @NonNull
+  private String updateFeed(long id, @NonNull Feed feed)
       throws RemoteException {
     ContentValues values = new ContentValues();
     String title = feed.getTitle();
@@ -247,6 +249,7 @@ class SyncWorker implements Runnable {
     if (provider.update(Provider.getUri(Provider.T_PODCAST, id), values, null, null) != 1) {
       throw new RemoteException("Failed to update database with new podcast data");
     }
+    return title;
   }
 
   @NonNull