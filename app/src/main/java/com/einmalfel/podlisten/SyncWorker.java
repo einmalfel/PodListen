2059ad7a79c02c3554ccb04272359f612c8f7064
Vasily Makarov <einmalfel@gmail.com>
2015-11-22 23:58:40 +0300

    SyncWorker: fix processing enclosures w/o mime type

--- app/src/main/java/com/einmalfel/podlisten/SyncWorker.java
+++ app/src/main/java/com/einmalfel/podlisten/SyncWorker.java
@@ -6,6 +6,7 @@ import android.os.RemoteException;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.text.Html;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.einmalfel.earl.EarlParser;
@@ -153,8 +154,8 @@ class SyncWorker implements Runnable {
     for (Enclosure enclosure : episode.getEnclosures()) {
       String type = enclosure.getType();
       String link = enclosure.getLink();
-      if ((type != null && AUDIO_PATTERN.matcher(type).matches()) ||
-          (type == null && urlPointsToAudio(link))) {
+      if ((!TextUtils.isEmpty(type) && AUDIO_PATTERN.matcher(type).matches()) ||
+          (TextUtils.isEmpty(type) && urlPointsToAudio(link))) {
         audioLink = link;
         audioSize = enclosure.getLength();
       }
